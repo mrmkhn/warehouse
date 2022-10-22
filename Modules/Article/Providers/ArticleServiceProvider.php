7cc762bcaf60a12c1f7c6a78e040f49763624c85
mrmkhn <khani.mrm@gmail.com>
2022-10-22 16:35:23 +0330

    add migrations and create tables(products & articles)

--- Modules/Article/Providers/ArticleServiceProvider.php
+++ Modules/Article/Providers/ArticleServiceProvider.php
@@ -25,7 +25,7 @@ class ArticleServiceProvider extends ServiceProvider
     public function boot()
     {
 //        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
-//        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
 
     }
 }
